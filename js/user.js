278ecbf34a98e8c081e79dcac7a45a271ba5530c
Monzoor Morshed <mmtamal@gmail.com>
2015-09-06 01:28:29 +0600

    added files

---
+++ js/user.js
@@ -0,0 +1,33 @@
+// $( document ).ready(function() {
+// 	$("section").height($(window).height());
+
+// 	 var stickyNav = function () {
+//         var scrollTop = $(window).scrollTop();
+//         if (scrollTop > 300) {
+//             $('header').addClass('selected');
+//         } else {
+//             $('header').removeClass('selected');
+//         }
+//     };
+       
+    
+
+//     $(window).scroll(function () {
+//         stickyNav();
+//     });
+
+//      $(".index li a").click(function(event) {
+//         event.preventDefault();
+//         console.log();
+//         $('html, body').animate({
+//             scrollTop: $("#"+$(this).data("nav")).offset().top
+//         }, 500);
+//     });
+// })
+$(document).ready(function () {
+
+    $(".player").mb_YTPlayer().on("YTPUnstarted", function(e){
+        console.log("ready");
+    });
+
+});
