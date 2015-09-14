7a99dd48e489c5911fe70b26ae877a13e08090e7
Monzoor Morshed <mmtamal@gmail.com>
2015-09-15 01:53:47 +0600

    added changes

--- js/user.js
+++ js/user.js
@@ -37,5 +37,25 @@ $(document).ready(function () {
         console.log("changed");
     }.bind(this));
 
+    var theForm = document.getElementById('theForm');
+
+    new stepsForm(theForm, {
+        onSubmit : function (form) {
+            // hide form
+            classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
+
+            /*
+            form.submit()
+            or
+            AJAX request (maybe show loading indicator while we don't have an answer..)
+            */
+
+            // let's just simulate something...
+            var messageEl = theForm.querySelector( '.final-message' );
+            messageEl.innerHTML = 'Thank you! We\'ll be in touch.<br><a href="" class="btn ask btn-lg">Ask briefcase</a>';
+            classie.addClass( messageEl, 'show' );
+        }
+    } );
+
 });
 
